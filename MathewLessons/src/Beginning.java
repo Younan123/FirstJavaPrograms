0103ed77511b044827020e5f49f5e3345d82c2e5
Evan-Younan <Evanyounan@mail.utoronto.ca>
2018-06-05 17:39:13 -0400

    Commit

--- MathewLessons/src/Beginning.java
+++ MathewLessons/src/Beginning.java
@@ -4,5 +4,9 @@ public class Beginning {
 	public Beginning() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Mathew");
+	}
 
 }
